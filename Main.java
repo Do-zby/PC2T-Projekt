d6ac7e1da558d01756a7dde75358ae6d35c7c172
Bruthovic <145433321+Bruthovic@users.noreply.github.com>
2024-04-05 14:52:48 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -18,7 +18,10 @@ public class Main
 			System.out.println("Zvoľte požadovaný úkon:");
 			System.out.println("1 --- Pridanie novej knihy");
 			System.out.println("2 --- Úprava knihy");
-			System.out.println("3 --- Výpis všetkých kníh v databáze");
+			System.out.println("3 --- Zmazanie knihy");
+			System.out.println("4 --- Úprava dostupnosti");
+			System.out.println("5 --- Výpis všetkých kníh v databáze");
+			System.out.println("6 --- Výpis informácií o konkrétnej knihe");
 			System.out.println("8 --- Ukončenie programu");
 
 			vyber = sc.nextInt();
@@ -74,10 +77,33 @@ public class Main
 				mojaDatabaza.upravaKnihy(názov,autori,rok,Vypozicanie);
 				break;
 			case 3:
+				System.out.println("Zadajte názov knihy, ktorú chcete odstraniť");
+				názov=sc.next();
+				mojaDatabaza.odstranenieKnihy(názov);
+				break;
+			case 4:
+				System.out.println("Zadajte názov knihy, ktorej chcete zmeniť dostupnosť");
+				názov=sc.next();
+				System.out.println("Zmeňte dostupnosť (Y-Vrátená, N-Vypožičaná");
+				Vypozicanie=sc.next();
+				mojaDatabaza.zmenaDostupnosti(názov, Vypozicanie);
+			case 5:
 				mojaDatabaza.vypisDatabázy();
-
+				break;
+			case 6:
+				//hladanie knihy
+				break;
+			case 7:
+				//vypisanie knih podla autora
 				break;
 			case 8:
+				//vypisanie knih podla zanru
+				break;
+			case 9:
+				//vypisanie vypozicanich knih podla toho, ci sa jedna o roman alebo ucebnicu
+				break;
+			case 13:
+				System.out.println("Dovidenia.");
 				run=false;
 				break;
 				}
